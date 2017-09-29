a4243cdbbf1de7d0e4a5d9c10a1bc0f22631583b
qjames65 <q.james65@gmail.com>
2017-09-29 03:56:38 +0000

    automatically backed up by learn

--- b.js
+++ b.js
@@ -1,12 +1,12 @@
 var a = require("./a.js")
 
 if (a.name === "Susan") {
-console.log "The name is correct."
+console.log("The name is correct.")
 } else {
     console.log("Expected: Susan, Received: "+a.name)
 }
 if (a.height=== "70") {
-console.log "The height is correct."
+console.log("The height is correct.")
 } else {
     console.log("Expected: 70, Received: "+a.height)
 }
